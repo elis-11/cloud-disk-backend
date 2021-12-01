c7dfaae1ff9405873ceb86f461f16b15012885f7
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-01 09:49:27 +0100

    second step

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 import express from 'express';
- 
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 const PORT = 5001;
- 
-app.get("/test", (req, res) => {
-    res.send('this is an endpoint test');
+
+app.use(cookieParser());
+app.use(
+	session({
+		resave: true,
+		saveUninitialized: true,
+		secret: 'secret'
+	})
+);
+
+app.get('/test', (req, res) => {
+	res.send('this is an endpoint test-11');
 });
- 
+
 app.listen(PORT, (req, res) => {
-    console.log(`API listening on port http://localhost:${PORT}`);
+	console.log(`API listening on port http://localhost:${PORT}`);
 });
