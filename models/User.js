0aee3cee71e6308d1040358dc4fff08ef3c44331
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-21 18:25:20 +0100

    creating new User

---
+++ models/User.js
@@ -0,0 +1,9 @@
+const { Schema, model, Types } = require("mongoose");
+
+const Schema = new Schema({
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  todos: [{type: Types.ObjectId, ref: 'Todo'}]
+});
+
+module.exports = model("User", Schema);
---
+++ routes/auth.route.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const router = Router();
+const User = require("../models/User");
+
+router.post("/registration", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const isUsed = await User.findOne({ email });
+
+    if (isUsed) {
+      return res
+        .status(300)
+        .json({ message: "This Email is already used! Try anoder." });
+    }
+
+    const user = new User({
+      email,
+      password,
+    });
+
+await user.save();
+
+res.status(201).json({ message: "User is created"})
+
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+module.exports = router;
