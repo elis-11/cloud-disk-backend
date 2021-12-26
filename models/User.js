f9c98e27b7a516f62210dbd8fc8bd21be2a9535f
elis-11 <eliza.arzanukaeva@gmail.com>
2021-12-26 15:11:51 +0100

    start new app

--- models/User.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model, Types } = require("mongoose");
-
-const Schema = new Schema({
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
-  todos: [{type: Types.ObjectId, ref: 'Todo'}]
-});
-
-module.exports = model("User", Schema);
